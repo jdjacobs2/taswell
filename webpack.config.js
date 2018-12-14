6cb9aaade0e76c9996509c69ebe9ebdeaa915f4a
James Jacobs <jim@jacobshome.com>
2018-12-14 13:58:37 -0500

    working site based upon example videos

---
+++ webpack.config.js
@@ -0,0 +1,68 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+  module.exports = {
+    entry: './src/js/index.js',
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+      // publicPath: '/dist'  //only needed if html does not reside in dist folder
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader'
+            // options: {
+            //   presets: ['@babel/preset-env']
+            // }
+          }
+        },
+        {
+          test: /\.(scss|css)$/,
+          use: [
+            {
+              loader: 'style-loader'
+            },
+            {
+              loader: 'css-loader'
+            },
+            {
+              loader: 'sass-loader'
+            }
+          ]
+        },
+        {
+          test: /\.(png|svg|jpg|gif)$/,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                name: '[name].[ext]',
+                outputPath: 'img/',
+                publicPath: 'img/'
+              }
+            }
+            // {
+            //   loader: 'image-webpack-loader' //optimizes image size--but breaks compile
+            // }
+          ]
+        },
+        {
+          test: /\.html$/,
+          use: ['html-loader']
+        }
+    ]
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: 'src/index.html'
+      }),
+      new CleanWebpackPlugin(['dist'])  //removes dist folder before building
+    ]
+};
+
