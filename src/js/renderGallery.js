502b495f56fb6b613711770fb30580b3a3dcddc1
James Jacobs <jim@jacobshome.com>
2019-01-27 13:30:58 -0500

    not working--need to add data-set attribute

--- src/js/renderGallery.js
+++ src/js/renderGallery.js
@@ -3,36 +3,49 @@ import path from 'path';
 import camels from "../img2/camels-desert-landscape-53537.jpg";
 import desert from "../img2/daylight-desert-drought-459319.jpg";
 
-let thumbnailList;
-const load = ()=> {
+let thumbnailList, accordion, photo;
+
+const load = () => {
   thumbnailList = document.querySelector('#thumbnails');
-  safety = document.querySelector('#safety');
-}
+  accordion = document.querySelector('#accordionEx');
+  photo = document.querySelector('#photo');
 
 window.onload = load;
 
-safety.addEventListener('click', () => {
-  // alert('safety clicked');
-  const safetyPhotos = document.querySelector('#safetyPhotos');
-  renderGallery(data,safetyPhotos);
-});
+
+  // photo.addEventListener('click', () => {
+  //   // alert('photo clicked');
+  //   const photoPhotos = document.querySelector('#photoPhotos')
+  //   renderGallery(data, photoPhotos);
+  // });
+
+  accordion.addEventListener('click', (e) => {
+    let el = e.target.dataset.action;
+    // alert('clicked child of accordion: ' + JSON.stringify(el));
+    if (el === 'photo') {
+     renderGallery(data, photoPhotos);
+   }
+  });
+}
+
+
 
 const pathToImages = path.resolve(__dirname, './img/camels-desert-landscape-53537.jpg');  
 
 // Not yet used:
-const images = [
-  "http://media.digitalcameraworld.com/files/2012/10/Vertorama_landscape_photography_tips_PHO17.insight02and03.vertorama.jpg",
-  "http://media.digitalcameraworld.com/files/2012/11/Black_and_white_landscape_photography_DCM131.shoot_creative.main_image_RGB.jpg",
-  "http://media.digitalcameraworld.com/files/2012/10/Vertorama_landscape_photography_tips_PHO17.insight02and03.vertorama.jpg",
-  "http://media.digitalcameraworld.com/files/2012/10/Vertorama_landscape_photography_tips_PHO17.insight02and03.vertorama.jpg",
-  "http://i.kinja-img.com/gawker-media/image/upload/s--v1mMDXQY--/c_fit,fl_progressive,q_80,w_636/otveb9dlpcz16you6de9.jpg"
-];
+// const images = [
+//   "http://media.digitalcameraworld.com/files/2012/10/Vertorama_landscape_photography_tips_PHO17.insight02and03.vertorama.jpg",
+//   "http://media.digitalcameraworld.com/files/2012/11/Black_and_white_landscape_photography_DCM131.shoot_creative.main_image_RGB.jpg",
+//   "http://media.digitalcameraworld.com/files/2012/10/Vertorama_landscape_photography_tips_PHO17.insight02and03.vertorama.jpg",
+//   "http://media.digitalcameraworld.com/files/2012/10/Vertorama_landscape_photography_tips_PHO17.insight02and03.vertorama.jpg",
+//   "http://i.kinja-img.com/gawker-media/image/upload/s--v1mMDXQY--/c_fit,fl_progressive,q_80,w_636/otveb9dlpcz16you6de9.jpg"
+// ];
 
 const data = [
-  ['1', "colored landscapte", "http://media.digitalcameraworld.com/files/2012/10/Vertorama_landscape_photography_tips_PHO17.insight02and03.vertorama.jpg"],
-  ["2", "black & white photo", "http://media.digitalcameraworld.com/files/2012/11/Black_and_white_landscape_photography_DCM131.shoot_creative.main_image_RGB.jpg"],
-  ["3", "colored desert", camels],
-  ["4", "colored desert2", desert]
+  ["colored landscapte", "http://media.digitalcameraworld.com/files/2012/10/Vertorama_landscape_photography_tips_PHO17.insight02and03.vertorama.jpg"],
+  ["black & white photo", "http://media.digitalcameraworld.com/files/2012/11/Black_and_white_landscape_photography_DCM131.shoot_creative.main_image_RGB.jpg"],
+  ["colored desert", camels],
+  ["colored desert2", desert]
 ];
 
 console.log(`the loaction of renderGallery is = ${__dirname} and path to images is ${pathToImages}`);
@@ -43,12 +56,12 @@ const renderGallery = (data, location) => {
   location.appendChild(ul);
 
 
-  data.forEach((el) => {  // el = {caption, href, title, img}  alt = caption
-    renderThumbnail(el, ul);
+  data.forEach((el, index) => {  // el = {title, img} 
+    renderThumbnail(el, index + 1, ul);
   });
 
-  data.forEach((el) => {  // el = {caption, href, title, img}  alt = caption
-    renderLightBoxPhoto(el, thumbnailList, data.length);
+  data.forEach((el, index) => {  // el = {title, img}  a
+    renderLightBoxPhoto(el, index + 1, location, data.length);
   });
   
 };
--- src/js/renderItem.js
+++ src/js/renderItem.js
@@ -1,20 +1,21 @@
-// photo = {caption, href, title, img}  alt = caption
+// photo = {title, img}  alt attribute of src element = title
 import thumbnailList from './renderGallery'; // <sector class='top2'
 
-export const renderThumbnail = (photo, location) => {
+export const renderThumbnail = (photo, index, location) => {
   const li = document.createElement('li');
   li.classList = 'galleryItem';
   const markup = `
-      <a class="caption" href="#${photo[0]}" data-title="${photo[1]}-title" data-description="${photo[1]}-desc">
-        <img src="${photo[2]}" alt='${photo[1]}'>
+      <a class="caption" href="#${index}" data-title="${photo[0]}-title" data-description="${photo[0]}-desc">
+        <img src="${photo[1]}" alt='${photo[0]}'>
       </a>
   `; 
   li.innerHTML = markup;
   location.appendChild(li);
 };
 
-export const renderLightBoxPhoto = (item, location, length) => {
-  let currentItem = parseInt(item[0]);
+export const renderLightBoxPhoto = (item, index, location, length) => {
+  let currentItem = index;
+  index = index.toString();
   // console.log(`currentItem = ${currentItem} and item = *${item[0]}* and length = *${length}* and type = ${typeof item[0]}`)
   let previousItem;
   let nextItem;
@@ -30,18 +31,19 @@ export const renderLightBoxPhoto = (item, location, length) => {
   }
   previousItem = previousItem.toString(10);
   nextItem = nextItem.toString(10);
+  const div = document.createElement('div');
+  div.id = `${index}`;
+  div.classList = 'lightbox'
   const markup = `
-    <div class="lightbox" id="${item[0]}"> 
-      <div class="image">
-          <img src="${item[2]}">
-              <a href="#${previousItem}" class="prev"></a>
-              <a href="#top2" class="close"></a>
-              <a href="#${nextItem}" class="next"></a>
-      </div>
+    <div class="image">
+        <img src="${item[1]}">
+            <a href="#${previousItem}" class="prev"></a>
+            <a href="#top2" class="close"></a>
+            <a href="#${nextItem}" class="next"></a>
     </div>
   `;
-
- location.insertAdjacentHTML('beforeend', markup);
+  div.innerHTML = markup;
+  location.appendChild(div);
 }; 
 
 
