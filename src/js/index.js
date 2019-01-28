18beefa633aaf84e7195c02ee05b8f1693fee345
James Jacobs <jim@jacobshome.com>
2019-01-28 10:57:40 -0500

    background and spacing changes

--- src/js/index.js
+++ src/js/index.js
@@ -6,9 +6,9 @@ import './renderItem.js'
 import '../scss/index.scss';
 import '../scss/equipment.scss';
 import '../scss/lightbox.scss'
-import '../img/3933.jpg';
-import '../img/3951.jpg';
-import '../img/3936e.jpg';
-import '../img/3947.jpg';
+// import '../img/3933.jpg';
+// import '../img/3951.jpg';
+// import '../img/3936e.jpg';
+// import '../img/3947.jpg';
 import '../img/tin-can-phone.jpg'
 
