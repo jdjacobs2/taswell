635d2805300335386b512927918f8425c74d5d42
James Jacobs <jim@jacobshome.com>
2019-01-27 21:34:31 -0500

    working with photos under seperate heading

--- src/js/index.js
+++ src/js/index.js
@@ -5,7 +5,7 @@ import './renderGallery.js'
 import './renderItem.js'
 import '../scss/index.scss';
 import '../scss/equipment.scss';
-import '../scss/lightbox.css'
+import '../scss/lightbox.scss'
 import '../img/3933.jpg';
 import '../img/3951.jpg';
 import '../img/3936e.jpg';
