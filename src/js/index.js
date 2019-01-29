66ea9b0bf871ebfc0074152e01d7f6ebcb2a78de
James Jacobs <jim@jacobshome.com>
2019-01-28 19:28:40 -0500

    Error free compilation and totally works apparently

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,3 @@
-
 import 'bootstrap';
 import "./equipment";
 import './renderGallery.js'
@@ -6,9 +5,8 @@ import './renderItem.js'
 import '../scss/index.scss';
 import '../scss/equipment.scss';
 import '../scss/lightbox.scss'
-// import '../img/3933.jpg';
-// import '../img/3951.jpg';
-// import '../img/3936e.jpg';
-// import '../img/3947.jpg';
+import '../img/IMG_3933.jpg';
+import '../img/IMG_3951.jpg';
+import '../img/IMG_3936e.jpg';
+import '../img/IMG_3947.jpg';
 import '../img/tin-can-phone.jpg'
-
