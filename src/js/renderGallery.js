bb3f1800e0f8a200eeb4a0f59f33dc013f51f420
James Jacobs <jim@jacobshome.com>
2019-01-29 08:21:19 -0500

    modified landing page photos

--- src/js/renderGallery.js
+++ src/js/renderGallery.js
@@ -1,7 +1,4 @@
 import {renderThumbnail, renderLightBoxPhoto} from "./renderItem";
-import path from 'path';
-// import camels from "../img2/camels-desert-landscape-53537.jpg";
-// import desert from "../img2/daylight-desert-drought-459319.jpg";
 
 import IMG_20180105_125501 from '../img3/IMG_20180105_125501.jpg';
 import IMG_2146 from '../img3/IMG_2146.jpg';
@@ -46,11 +43,6 @@ import P1010023 from '../img3/P1010023.jpg';
 import P1010027 from '../img3/P1010027.jpg';
 import P1010036 from '../img3/P1010036.jpg';
 
-
-
-
-
-
 let thumbnailList, accordion, photoPhotos;
 
 const load = () => {
@@ -58,12 +50,8 @@ const load = () => {
   accordion = document.querySelector('#accordionEx');
   photoPhotos = document.querySelector('#photoPhotos');
 
-
-
-
   accordion.addEventListener('click', (e) => {
     let el = e.target.dataset.action;
-    // alert('clicked child of accordion: ' + JSON.stringify(el));
     if (el === 'photo') {
       renderGallery(data, photoPhotos);
     }
@@ -71,258 +59,6 @@ const load = () => {
 }
 
 window.onload = load;
-const pathToImages = path.resolve(__dirname, './img/camels-desert-landscape-53537.jpg');  
-console.log(`the loaction of renderGallery is = ${__dirname} and path to images is ${pathToImages}`);
-
-// const data = [
-//   ["colored landscapte", "http://media.digitalcameraworld.com/files/2012/10/Vertorama_landscape_photography_tips_PHO17.insight02and03.vertorama.jpg"],
-//   ["black & white photo", "http://media.digitalcameraworld.com/files/2012/11/Black_and_white_landscape_photography_DCM131.shoot_creative.main_image_RGB.jpg"],
-//   ["colored desert", camels],
-//   ["colored desert2", desert]
-// ];
-
-
-
-// const data = [{
-//   "SourceFile": "IMG_20180105_125501.jpg",
-//   "Title": "Windsong View Aft Sailing",
-//   "Description": "hull"
-// },
-// {
-//   "SourceFile": "IMG_2146.jpg",
-//   "Title": "Windsong at Anchor",
-//   "Description": "hull"
-// },
-// {
-//   "SourceFile": "IMG_3752.jpg",
-//   "Title": "Navigation Station",
-//   "Description": "nav"
-// },
-// {
-//   "SourceFile": "IMG_3755.jpg",
-//   "Title": "Windsong Transom View",
-//   "Description": "hull"
-// },
-// {
-//   "SourceFile": "IMG_3758.jpg",
-//   "Title": "Windsong Adventuras View",
-//   "Description": "hull"
-// },
-// {
-//   "SourceFile": "IMG_3762.jpg",
-//   "Title": "Windsong at Adventuras Dock",
-//   "Description": "hull"
-// },
-// {
-//   "SourceFile": "IMG_3773-2.jpg",
-//   "Title": "62 STAndersen above deck electric winches ",
-//   "Description": "sails"
-// },
-// {
-//   "SourceFile": "IMG_3773.jpg",
-//   "Title": "62 STAndersen above deck electric winches ",
-//   "Description": "hull"
-// },
-// {
-//   "SourceFile": "IMG_3774e.jpg",
-//   "Title": "40 ST Andersen above deck electric winches ",
-//   "Description": "sails"
-// },
-// {
-//   "SourceFile": "IMG_3782.jpg",
-//   "Title": "Galley",
-//   "Description": "galley"
-// },
-// {
-//   "SourceFile": "IMG_3783.jpg",
-//   "Title": "Galley",
-//   "Description": "galley"
-// },
-// {
-//   "SourceFile": "IMG_3793.jpg",
-//   "Title": "Windsong Sailing Downwind at Night",
-//   "Description": "hull"
-// },
-// {
-//   "SourceFile": "IMG_3840.jpg",
-//   "Title": "Windsong on Hard",
-//   "Description": "hull"
-// },
-// {
-//   "SourceFile": "IMG_3860-2.jpg",
-//   "Title": "Bow Thruster Propeller",
-//   "Description": "mech"
-// },
-// {
-//   "SourceFile": "IMG_3860.jpg",
-//   "Title": " Lewmar 250T Bow Thruster Propeller",
-//   "Description": "mech"
-// },
-// {
-//   "SourceFile": "IMG_3866.jpg",
-//   "Title": "Max-Prop Classic Feathering Propeller",
-//   "Description": "mech"
-// },
-// {
-//   "SourceFile": "IMG_3921.jpg",
-//   "Title": "Salon Liquor Cabinet",
-//   "Description": "salon"
-// },
-// {
-//   "SourceFile": "IMG_3947-Edit.jpg",
-//   "Title": "Garmin Autopilot Class B Compact Drive Unit",
-//   "Description": "hull"
-// },
-// {
-//   "SourceFile": "IMG_4044-Edit.jpg",
-//   "Title": "Tashing Brass Threshold Plate",
-//   "Description": "hull"
-// },
-// {
-//   "SourceFile": "IMG_4048.jpg",
-//   "Title": "Foredeck viewed from  Dodger",
-//   "Description": "hull"
-// },
-// {
-//   "SourceFile": "IMG_4054.jpg",
-//   "Title": "Rigid Boom Vang",
-//   "Description": "sails"
-// },
-// {
-//   "SourceFile": "IMG_4059.jpg",
-//   "Title": "Maxwell 3500 Windless",
-//   "Description": "mech"
-// },
-// {
-//   "SourceFile": "IMG_4061.jpg",
-//   "Title": "Stainless Steel Cleet and Toe Rail",
-//   "Description": "hull"
-// },
-// {
-//   "SourceFile": "IMG_4064.jpg",
-//   "Title": "Hard Dodger",
-//   "Description": "hull"
-// },
-// {
-//   "SourceFile": "IMG_4074.jpg",
-//   "Title": "Andersen 40 Electric Stainless Stell Variable Speed Winch",
-//   "Description": "sails"
-// },
-// {
-//   "SourceFile": "IMG_4080.jpg",
-//   "Title": "Andersen 62 Electric Stainless Stell Variable Speed Winch",
-//   "Description": "sails"
-// },
-// {
-//   "SourceFile": "IMG_4082.jpg",
-//   "Title": "Engine Room Fan Vent",
-//   "Description": "mech"
-// },
-// {
-//   "SourceFile": "IMG_4086.jpg",
-//   "Title": "Cockpit From Stern(w/o cushions)",
-//   "Description": "hull"
-// },
-// {
-//   "SourceFile": "IMG_4088.jpg",
-//   "Title": "Genoa Sheet Truning Block",
-//   "Description": "sails"
-// },
-// {
-//   "SourceFile": "IMG_4093.jpg",
-//   "Title": "Fiberglass Propane Tank",
-//   "Description": "hull"
-// },
-// {
-//   "SourceFile": "IMG_4094.jpg",
-//   "Title": "Andersen Traveller Winch",
-//   "Description": "sails"
-// },
-// {
-//   "SourceFile": "IMG_4101.jpg",
-//   "Title": "Windsong Hull Port Front View",
-//   "Description": "hull"
-// },
-// {
-//   "SourceFile": "IMG_4135.jpg",
-//   "Title": "Dorade",
-//   "Description": "hull"
-// },
-// {
-//   "SourceFile": "IMG_4141.jpg",
-//   "Title": "Hard Dodger from Aft",
-//   "Description": "hull"
-// },
-// {
-//   "SourceFile": "IMG_4142.jpg",
-//   "Title": "Cockpit Table",
-//   "Description": "hull"
-// },
-// {
-//   "SourceFile": "IMG_4143.jpg",
-//   "Title": "Yanmar Motor Control Panel",
-//   "Description": "mech"
-// },
-// {
-//   "SourceFile": "IMG_4147.jpg",
-//   "Title": "Flag Pole",
-//   "Description": "hull"
-// },
-// {
-//   "SourceFile": "IMG_4149.jpg",
-//   "Title": "BBQ",
-//   "Description": "hull"
-// },
-// {
-//   "SourceFile": "IMG_4150.jpg",
-//   "Title": "Garbaud Motor Crane",
-//   "Description": "mech"
-// },
-// {
-//   "SourceFile": "IMG_4153.jpg",
-//   "Title": "Cockpit with Cusiions",
-//   "Description": "hull"
-// },
-// {
-//   "SourceFile": "IMG_4154.jpg",
-//   "Title": "Foredeck from behind Cockpit",
-//   "Description": "hull"
-// },
-// {
-//   "SourceFile": "IMG_4178-Edit.jpg",
-//   "Title": "Windsong Starbord Hull",
-//   "Description": "hull"
-// },
-// {
-//   "SourceFile": "IMG_4198.jpg",
-//   "Title": "V-Birth Under Mattress Mat",
-//   "Description": "guest"
-// },
-// {
-//   "SourceFile": "IMG_4201.jpg",
-//   "Title": "Bow Thruster Motor & Batteries",
-//   "Description": "mech"
-// },
-// {
-//   "SourceFile": "P1010023.jpg",
-//   "Title": "Windsong Sailing",
-//   "Description": "hull"
-// },
-// {
-//   "SourceFile": "P1010027.jpg",
-//   "Title": "Windsong Sailing from Front Quarter",
-//   "Description": "hull"
-// },
-// {
-//   "SourceFile": "P1010036.jpg",
-//   "Title": "Windsong Sailing from Aft",
-//   "Description": "hull"
-// },
-// {
-//   "SourceFile": "taswell_49_drawing--Windsong.jpg",
-//   "Title": "Windsong Plans",
-//   "Description": "hull"
-// }];
 
 
 const data = [
@@ -375,9 +111,7 @@ const renderGallery = (data, location) => {
   ul.classList = 'galleryList';
   location.appendChild(ul);
 
-
   data.forEach((el, index) => {  // el = {image, description, category} 
-    // el.SourceFile = sourcePath(el);
     renderThumbnail(el, index + 1, ul);
   });
 
@@ -386,7 +120,3 @@ const renderGallery = (data, location) => {
   });
   
 };
-
-// const sourcePath = function(el) {
-//   return `../img3/${el.SourceFile}`;
-// }
