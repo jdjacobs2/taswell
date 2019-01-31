9d04ffae14d7e69b09099058e3d8f7af095a3f05
James Jacobs <jim@jacobshome.com>
2019-01-31 11:29:06 -0500

    added mini-css-extract-plugin to have css load before DOM

--- webpack.config.js
+++ webpack.config.js
@@ -2,91 +2,100 @@ const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-  module.exports = {
-    entry: './src/js/index.js',
-    output: {
-      filename: 'bundle.js',
-      path: path.resolve(__dirname, 'dist'),
-      // publicPath: '/dist'  //only needed if html does not reside in dist folder
-    },
-    devtool: 'inline-source-map',
-    devServer: {
-      contentBase: './dist',
-      hot: true,
-      open: true
-    },
-    module: {
-      rules: [
-    {
-      test: /\.js$/,
-      exclude: /(node_modules|bower_components)/,
-      use: {
-    loader: 'babel-loader'
-    // options: {
-    //   presets: ['@babel/preset-env']
-    // }
+module.exports = {
+  entry: './src/js/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    // publicPath: '/dist'  //only needed if html does not reside in dist folder
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+    hot: true,
+    open: true
+  },
+  module: {
+    rules: [{
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader'
+          // options: {
+          //   presets: ['@babel/preset-env']
+          // }
+        }
+      },
+      {
+        // https://stackoverflow.com/questions/49410921/webpack-4-mini-css-extract-plugin-sass-loader-splitchunks
+        // https://github.com/webpack-contrib/mini-css-extract-plugin
+        // TODO: study above for production use-minify etc
+        test: /\.(scss|css)$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader'
+            // options: {
+            //   modules: true,
+            //   sourceMap: true,
+            //   importLoader:2
+            // }
+          },
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [{
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'img/',
+              publicPath: 'img/'
+            }
+          }
+          // {
+          //   loader: 'image-webpack-loader' //optimizes image size--but breaks compile
+          // }
+        ]
+      },
+      {
+        test: /\.html$/,
+        use: ['html-loader']
+      },
+      {
+        test: /\.(eot|woff|woff2|ttf|svg)(\?\S*)?$/,
+        use: [{
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            outputPath: 'fonts/',
+            publicPath: 'fonts/'
+          }
+        }]
       }
-    },
-    {
-      test: /\.(scss|css)$/,
-      use: [
-    {
-      loader: 'style-loader'
-    },
-    {
-      loader: 'css-loader'
-    },
-    {
-      loader: 'sass-loader'
-    }
-      ]
-    },
-    {
-      test: /\.(png|svg|jpg|gif)$/,
-      use: [
-    {
-      loader: 'file-loader',
-      options: {
-    name: '[name].[ext]',
-    outputPath: 'img/',
-    publicPath: 'img/'
-      }
-    }
-    // {
-    //   loader: 'image-webpack-loader' //optimizes image size--but breaks compile
-    // }
-      ]
-    },
-    {
-      test: /\.html$/,
-      use: ['html-loader']
-    },
-    {
-      test: /\.(eot|woff|woff2|ttf|svg)(\?\S*)?$/,
-      use: [{
-    loader: 'file-loader',
-    options: {
-      name: '[name].[ext]',
-      outputPath: 'fonts/',
-      publicPath: 'fonts/'
-    }
-      }]
-    }
-    // {
-    //   test: /font-awesome\.config\.js/,
-    //   use: [
-    //     { loader: 'style-loader' },
-    //     { loader: 'font-awesome-loader' }
-    //   ]
-    // }
+      // {
+      //   test: /font-awesome\.config\.js/,
+      //   use: [
+      //     { loader: 'style-loader' },
+      //     { loader: 'font-awesome-loader' }
+      //   ]
+      // }
     ]
-    },
-    plugins: [
-      new HtmlWebpackPlugin({
-    template: 'src/index.html'
-      }),
-      new CleanWebpackPlugin(['dist'])  //removes dist folder before building
-    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    }),
+    new CleanWebpackPlugin(['dist']), //removes dist folder before building
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: "style.css",
+      chunkFilename: "[name].css"
+    })
+
+  ]
 };
-
